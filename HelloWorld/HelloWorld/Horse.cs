6f1d531ff84b45f882a2aae7ef45f84da4de2aae
Stefanescu Eduard-Traian <eduardstefanescu22@gmail.com>
2017-11-23 17:34:52 +0200

    Homework for Session1

---
+++ HelloWorld/HelloWorld/Horse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Horse : Animal
+    {
+        public override string Name()
+        {
+            return "Horse";
+        }
+        public override string Noise()
+        {
+            return "neigh";
+        }
+    }
+}

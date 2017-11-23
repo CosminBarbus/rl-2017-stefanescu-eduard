8243a185b44a0b6e4ed1e7415dc3ff13e369edfd
Stefanescu Eduard-Traian <eduardstefanescu22@gmail.com>
2017-11-23 16:44:27 +0200

    Homework Session 1

---
+++ HelloWorld/Session1/Session1/Lion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session1
+{
+    class Lion : Animal
+    {
+        public override string Name()
+        {
+            return "Lion";
+        }
+        public override string Noise()
+        {
+            return "roar";
+        }
+    }
+}
---
+++ HelloWorld/Session1/Session1/Sound.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Session1
+{
+    class Sound
+    {
+        private string partialURL = "https://www.google.com/logos/fnbx/animal_sounds/";
+        private string completeURL;
+        public void MakeNoise(Animal animal)
+        {
+            completeURL = partialURL + animal.Name().ToLower() + ".mp3";
+            var sound = new WMPLib.WindowsMediaPlayer();
+            sound.URL = completeURL;
+            Console.WriteLine(string.Format("{0} makes {1}", animal.Name(), animal.Noise()));
+            Thread.Sleep(2500);
+        }
+    }
+}
